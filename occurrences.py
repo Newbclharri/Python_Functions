b24ca4e016298833a914639d05a5bcbc621c8d85
contactharrisc2 <contactharrisc2@gmail.com>
2022-07-06 18:19:03 -0500

    refactors occurrences.py

--- occurrences.py
+++ occurrences.py
@@ -1,3 +1,6 @@
+#############
+## FUNCTIONS
+#############
 def occurrences(string, substring):
     return string.count(substring)
 
@@ -8,20 +11,6 @@ def occurrences2(string, substring):
             count += 1
             string = string.replace(substring, "", 1)
     return count
-
-print(occurrences("fleep floop", "e"))
-print(occurrences("fleep floop", "p"))
-print(occurrences("fleep floop", "ee"))
-print(occurrences("fleep floop", "fe"))
-print(occurrences("fleep floop", "fl"))
-
-print("_________________________________________________\n")
-
-print(occurrences2("fleep floop", "e"))
-print(occurrences2("fleep floop", "p"))
-print(occurrences2("fleep floop", "ee"))
-print(occurrences2("fleep floop", "fe"))
-print(occurrences2("fleep floop", "fl"))
 
 def occurrences3(string, substring):
     count = 0
@@ -49,6 +38,24 @@ def occurrences3(string, substring):
             if string[i] == substring:
                 count += 1
         return count
+    
+#################
+## TEST FUNCTIONS
+#################
+
+print(occurrences("fleep floop", "e"))
+print(occurrences("fleep floop", "p"))
+print(occurrences("fleep floop", "ee"))
+print(occurrences("fleep floop", "fe"))
+print(occurrences("fleep floop", "fl"))
+
+print("_________________________________________________\n")
+
+print(occurrences2("fleep floop", "e"))
+print(occurrences2("fleep floop", "p"))
+print(occurrences2("fleep floop", "ee"))
+print(occurrences2("fleep floop", "fe"))
+print(occurrences2("fleep floop", "fl"))
 
 print("_________________________________________________\n")
 
